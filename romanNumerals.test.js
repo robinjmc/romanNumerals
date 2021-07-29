0965f08f1acc896c225d47a4006de56837225ce9
Robin Cunningham <robin.cunningham@thinkmoney.co.uk>
2021-07-29 17:59:59 +0100

    2s and 3s for the multi use numerals

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -4,8 +4,11 @@ describe("Instances of the I rune", () => {
     test("that a single I returns 1", () => {
         expect(romanToArabic("I")).toBe(1);
     });
-    test("that a two Is returns 2", () => {
+    test("that two Is returns 2", () => {
         expect(romanToArabic("II")).toBe(2);
+    });
+    test("that three Is returns 3", () => {
+        expect(romanToArabic("III")).toBe(3);
     });
 });
 
@@ -21,6 +24,12 @@ describe("Instances of the X rune", () => {
     test("that a single X returns 10", () => {
         expect(romanToArabic("X")).toBe(10);
     });
+    test("that two Xs return 20", () => {
+        expect(romanToArabic("XX")).toBe(20);
+    });
+    test("that three Xs return 30", () => {
+        expect(romanToArabic("XXX")).toBe(30);
+    });
 });
 
 
@@ -35,6 +44,12 @@ describe("Instances of the C rune", () => {
     test("that a single C returns 100", () => {
         expect(romanToArabic("C")).toBe(100);
     });
+    test("that two Cs returns 200", () => {
+        expect(romanToArabic("CC")).toBe(200);
+    });
+    test("that three Cs returns 300", () => {
+        expect(romanToArabic("CCC")).toBe(300);
+    });
 });
 
 
@@ -48,5 +63,11 @@ describe("Instances of the D rune", () => {
 describe("Instances of the M rune", () => {
     test("that a single M returns 1000", () => {
         expect(romanToArabic("M")).toBe(1000);
+    });
+    test("that two Ms return 2000", () => {
+        expect(romanToArabic("MM")).toBe(2000);
+    });
+    test("that three Ms return 3000", () => {
+        expect(romanToArabic("MMM")).toBe(3000);
     });
 });
